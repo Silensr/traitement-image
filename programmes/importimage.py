fcf490d771a3f9904e569dc990e1c0c0f541a6ab
Tay <vat92@outlook.fr>
2023-01-31 11:48:39 +0100

    séance 30/01

---
+++ programmes/importimage.py
@@ -0,0 +1,9 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+image_lena = plt.imread( '././images/hand.JPG') # Emplacement de l'image
+
+plt.imshow(image_lena) # Permet d'afficher l'image
+plt.show()
